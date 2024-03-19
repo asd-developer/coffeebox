c17a5b4f1a23d6476e8d2bf4feabc8c405bef9d2
ASD-DEVELOPER <andreduarte.profissional@gmail.com>
2024-03-19 20:25:16 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template data-theme="coffee" class="font-unica">
+    <header class="container w-full p-2">
+        <h1 class="font-unica text-xl font-bold text-center">COFFEEBOX</h1>
+    </header>
+
+    <main class="w-full h-full p-2">
+      <RouterView/>
+    </main>
+
+    <footer class="bg-gray-800 text-white py-4 sticky bottom-0">
+      <div class="container mx-auto px-6">
+        <p>&copy; 2024 Your Company. All rights reserved.</p>
+      </div>
+    </footer>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Unica+One&display=swap');
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+body {
+  margin: 0 auto;
+  background-color: rgb(127, 109, 96);
+}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+      fontFamily: {
+        'unica': ['Unica One', 'sans-serif'],
+      },
+      fontSize: {
+        sm: ['14px', '20px'],
+        base: ['16px', '24px'],
+        lg: ['20px', '28px'],
+        xl: ['24px', '32px'],
+      }
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ['coffee'], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
+  },
+}
+

59be0c6e2adb7d4e9521b6ce5c202c7e68cb53cc
ASD-DEVELOPER <andreduarte.profissional@gmail.com>
2024-03-29 21:39:21 +0000

    modal advancements

--- src/components/search.vue
+++ src/components/search.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="flex flex-row w-full">
-      <input type="text" placeholder="Type here" class="input input-bordered md:w-1/3 w-[80%] h-1mais0"/>
-      <button class="btn btn-primary ml-2 md:w-20 w-[20%] h-10">Add Coffee</button>
+      <input type="text" placeholder="Search" class="input input-bordered md:w-1/3 w-[80%]"/>
+      <button class="btn btn-primary ml-2 md:w-20 w-[20%] h-10" onclick="addBeanBag.showModal()">Add Coffee</button>
     </section>
   </template>
   
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,24 @@
 <template>
   <section class="flex flex-row w-[90%] mx-auto mb-5">
+    <dialog id="addBeanBag" class="modal">
+      <div class="modal-box">
+        <form method="dialog" class="flex flex-col gap-5">
+          <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+          <h3 class="font-bold text-lg">Add new coffee bag</h3>
+          <label class="input input-bordered flex items-center gap-2 font-bold">
+            Name:
+            <input type="text" class="grow font-normal" placeholder="" />
+          </label>
+          <div class="flex flex-row justify-between">
+            <input type="file" class="file-input file-input-bordered w-full max-w-xs" />
+            <button class="btn btn-primary">Save</button>
+          </div>
+        </form>
+      </div>
+      <form method="dialog" class="modal-backdrop">
+        <button>close</button>
+      </form>
+    </dialog>
     <Search />
   </section>
   <section class="flex flex-row w-full">
@@ -8,17 +27,15 @@
 </template>
 
 <script setup>
-import { ref ,onMounted} from 'vue'
-import {addCoffeeName, getCoffeeNames} from '@/indexdb/indexdb'
+import { ref, onMounted } from 'vue'
+import { addCoffeeName, getCoffeeNames } from '@/indexdb/indexdb'
 import Search from '@/components/search.vue'
 import CardGrid from '@/components/cardGrid.vue'
 
-const namesList = ref([]); // Initialize as an empty array
+const namesList = ref([]);
 
 onMounted(async () => {
   namesList.value = await getCoffeeNames();
-  console.log(namesList.value); // Log the value after it's fetched
+  console.log(namesList.value);
 });
 </script>
-
-<style></style>
